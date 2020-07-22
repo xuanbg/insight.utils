77a53bc0f5ada1a13d137b21f694563eb9e066cc
Brian <xan.brian@gmail.com>
2020-07-22 11:34:40 +0800

    up

--- src/main/java/com/insight/utils/pojo/MemberDto.java
+++ src/main/java/com/insight/utils/pojo/MemberDto.java
@@ -28,9 +28,19 @@ public class MemberDto implements Serializable {
     private Integer type;
 
     /**
-     * 成员名称
+     * 编码
+     */
+    private String code;
+
+    /**
+     * 名称
      */
     private String name;
+
+    /**
+     * 备注
+     */
+    private String remark;
 
     public String getId() {
         return id;
@@ -56,12 +66,28 @@ public class MemberDto implements Serializable {
         this.type = type;
     }
 
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
     }
 
     @Override
