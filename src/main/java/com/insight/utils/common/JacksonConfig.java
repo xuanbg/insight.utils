6dba88bf906c63bdad3d81263bd060937b3570f1
Brian <xan.brian@gmail.com>
2022-12-13 18:13:15 +0800

    up to 2.2.2

--- src/main/java/com/insight/utils/common/JacksonConfig.java
+++ src/main/java/com/insight/utils/common/JacksonConfig.java
@@ -3,11 +3,20 @@ package com.insight.utils.common;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.insight.utils.DateTime;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * @author 宣炳刚
@@ -22,11 +31,20 @@ public class JacksonConfig {
     public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
 
-        // 全局配置序列化返回 JSON 处理
-        SimpleModule simpleModule = new SimpleModule();
-        //JSON Long ==> String
+        // 时间格式
+        var module = new JavaTimeModule();
+        module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTime.DEFAULT_TIME_FORMATTER));
+        module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTime.DEFAULT_TIME_FORMATTER));
+
+        module.addSerializer(LocalDate.class, new LocalDateSerializer(DateTime.DEFAULT_DATE_FORMATTER));
+        module.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTime.DEFAULT_DATE_FORMATTER));
+        objectMapper.registerModule(module);
+
+        // Long -> String
+        var simpleModule = new SimpleModule();
         simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
         objectMapper.registerModule(simpleModule);
+
         return objectMapper;
     }
 }
--- src/main/java/com/insight/utils/pojo/base/BusinessException.java
+++ src/main/java/com/insight/utils/pojo/base/BusinessException.java
@@ -8,18 +8,49 @@ package com.insight.utils.pojo.base;
 public class BusinessException extends RuntimeException {
 
     /**
+     * 异常代码
+     */
+    private Integer code;
+
+    /**
      * 异常消息
      */
     private final String message;
 
     /**
      * 构造方法
+     *
      * @param message 异常消息
      */
-    public BusinessException(String message){
+    public BusinessException(String message) {
         this.message = message;
     }
 
+    /**
+     * 构造方法
+     *
+     * @param code    异常代码
+     * @param message 异常消息
+     */
+    public BusinessException(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    /**
+     * 获取错误代码
+     *
+     * @return 错误代码
+     */
+    public Integer getCode() {
+        return code;
+    }
+
+    /**
+     * 获取错误消息
+     *
+     * @return 错误消息
+     */
     @Override
     public String getMessage() {
         return message;
