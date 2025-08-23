d57fe54637e2e294febca55681dc0cdae517c7a6
Brian <xan.brian@gmail.com>
2025-08-23 10:19:46 +0800

    feat(auth): 添加第三方应用 OpenID 类
    - 新增 OpenId 类，用于存储第三方应用的 OpenID 信息
    - 包含应用 ID、OpenId 和服务类型等属性
    - 为认证模块增加了对第三方应用用户标识的支持

---
+++ src/main/java/com/insight/utils/pojo/auth/OpenId.java
@@ -0,0 +1,50 @@
+package com.insight.utils.pojo.auth;
+
+import com.insight.utils.pojo.base.BaseXo;
+
+/**
+ * @author 宣炳刚
+ * @date 2025/8/23
+ * @remark 第三方应用OpenID
+ */
+public class OpenId extends BaseXo {
+
+    /**
+     * 应用ID
+     */
+    private String appId;
+
+    /**
+     * OpenId
+     */
+    private String openId;
+
+    /**
+     * 服务类型
+     */
+    private String service;
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+}
